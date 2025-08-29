a3b9523bfe9b06f4718f71fdb1a7d0833c306b03
Darkless12 <dftlg@yahoo.com>
2025-08-29 15:28:04 +0100

    adjusted some inputs

--- Src/Parsing/monster_checking.c
+++ Src/Parsing/monster_checking.c
@@ -1,22 +1,36 @@
 #include "../../Inc/cube3d.h"
 
+int	test_open(char *tex)
+{
+	int		fd;
+
+	fd = open(tex, O_RDONLY);
+	if (fd < 0)
+	{
+		ft_printf_fd(STDERR_FILENO, "Error\nCube3d: \"%s\": ", tex);
+		ft_putendl_fd(strerror(errno), 2);
+		return (1);
+	}
+	close(fd);
+	if (test_mlx_texture(tex))
+		return (1);
+	return (0);
+}
+
 int	look_mon_tex(char *tex, char t)
 {
 	int		i;
-	int		fd;
 
+	if (!tex)
+		return (parse_error("Failed to allocate monster texture path\
+ for verification", NULL));
 	i = 0;
 	while (tex[i] != 'X')
 		i++;
 	tex[i] = 0 + '0';
 	while (tex[i] != '4')
 	{
-		fd = open(tex, O_RDONLY);
-		if (fd < 0)
-			return (ft_printf_fd(STDERR_FILENO, "Error\nCube3d: \"%s\": ", tex), \
-free(tex), ft_putendl_fd(strerror(errno), 2), 1);
-		close(fd);
-		if (test_mlx_texture(tex))
+		if (test_open(tex))
 			return (free(tex), 1);
 		tex[i] += 1;
 		if (tex[i] == '4' && tex[i - 2] != t)
