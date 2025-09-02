b8f5e9d9da8cde7174b6360e17ea1bc991bf7883
Darkless12 <dftlg@yahoo.com>
2025-09-02 18:49:40 +0100

    draw sprite now clips image if out of screen bounds

--- Src/Cube3D/draw_sprite.c
+++ Src/Cube3D/draw_sprite.c
@@ -8,7 +8,9 @@ void	draw_sprite_y(int col, int tex_x, t_sprite *img, t_cube *cube)
 	int				tex_y;
 
 	y = img->draw_s_y;
-	while (y < img->draw_e_y)
+	if (y < 0)
+		y = 0;
+	while (y < img->draw_e_y && y < WIND_HEIGHT)
 	{
 		d = (y - img->draw_s_y) * 256; 
 		tex_y = ((d * img->height) / img->sprt_h) / 256;
@@ -58,16 +60,12 @@ player->plane_x * img->sprite_y);
 (1 + img->trans_x / img->trans_y));
 	img->sprt_h = abs((int)(WIND_HEIGHT / img->trans_y));
 	img->sprt_w = abs((int)(WIND_HEIGHT / img->trans_y));
-	img->draw_s_y = (-img->sprt_h / 2) + (WIND_HEIGHT / 2);
-	if (img->draw_s_y < 0)
-		img->draw_s_y = 0;
-	img->draw_e_y = (img->sprt_h / 2) + (WIND_HEIGHT / 2);
-	if (img->draw_e_y >= WIND_HEIGHT)
-		img->draw_e_y = WIND_HEIGHT - 1;
-	img->draw_s_x = (-img->sprt_w / 2) + img->sprt_scrn_x;
+	img->draw_s_y = -img->sprt_h / 2 + WIND_HEIGHT / 2;
+	img->draw_e_y = img->sprt_h / 2 + WIND_HEIGHT / 2;
+	img->draw_s_x = -img->sprt_w / 2 + img->sprt_scrn_x;
 	if (img->draw_s_x < 0)
 		img->draw_s_x = 0;
-	img->draw_e_x = (img->sprt_w /2) + img->sprt_scrn_x;
+	img->draw_e_x = img->sprt_w /2 + img->sprt_scrn_x;
 	if (img->draw_e_x >= WIND_WIDTH)
 		img->draw_e_x = WIND_WIDTH - 1;
 	draw_sprite_x(img, cube);
